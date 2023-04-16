b4593f7fcbbf2c78c0119fdd1ddc13a75f782234
“mvaibhav131” <98808183+mvaibhav131@users.noreply.github.com>
2023-04-16 12:57:53 +0530

    Feat :sparkles: added inline style for heading

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <h1 style={{color: "blue",padding: "10px",
+        <h1 style={{color: "blue",padding: "10px",textAlign:"center",
       fontFamily: "Arial"}}>Exploding Kitten Game</h1>
         <HomePage />
       </Provider>
