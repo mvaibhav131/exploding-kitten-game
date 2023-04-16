6c37e2f0e745bd4e5a301a19f5d4a4bcadf9f6e8
“mvaibhav131” <98808183+mvaibhav131@users.noreply.github.com>
2023-04-16 11:56:05 +0530

    Feat :sparkles: added redux types

---
+++ src/types.js
@@ -0,0 +1,9 @@
+export const START_GAME = 'START_GAME';
+export const REMOVE_CARD = 'REMOVE_CARD';
+export const FLIPPED_CARD = 'FLIPPED_CARD';
+export const DEFUSE_CARD = 'DEFUSE_CARD';
+export const CAT_CARD = 'CAT_CARD';
+export const SHUFFLE_CARD = 'SHUFFLE_CARD';
+export const EXPLODING_KITTEN_CARD = 'EXPLODING_KITTEN_CARD';
+export const GAME_OVER = 'GAME_OVER';
+export const GAME_WON = 'GAME_WON';
